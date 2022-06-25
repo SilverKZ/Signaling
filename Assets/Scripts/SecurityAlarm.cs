6897c50715cefe7dfdef263fd7a18227ed98570d
SilverKZ <silverkaz@mail.ru>
2022-06-25 23:38:37 +0600

    Edits after review

--- Assets/Scripts/SecurityAlarm.cs
+++ Assets/Scripts/SecurityAlarm.cs
@@ -2,6 +2,70 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
+public class SecurityAlarm : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+    
+    private AudioSource _sound;
+    private Coroutine _volumeChangeRoutine;
+    private float _targetValume;
+    private int _direction;
+
+    private void Start()
+    {
+        _sound = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent<Player>(out Player player))
+        {
+            _sound.volume = 0f;
+            _sound.Play();
+            _targetValume = 1f;
+            _direction = 1;
+            CoroutineControl();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent<Player>(out Player player))
+        {
+            _targetValume = 0f;
+            _direction = -1;
+            CoroutineControl();
+        }
+    }
+
+    private void CoroutineControl()
+    {
+        if (_volumeChangeRoutine != null)
+        {
+            StopCoroutine(_volumeChangeRoutine);
+        }
+
+        _volumeChangeRoutine = StartCoroutine(VolumeChange());
+    }
+
+    private IEnumerator VolumeChange()
+    {
+        while (_sound.volume != _targetValume)
+        {
+            _sound.volume = Mathf.MoveTowards(_sound.volume, _sound.maxDistance, _speed * _direction * Time.deltaTime);
+            yield return null;
+        }
+    }
+}
+
+
+/*
+ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
 public class SecurityAlarm : MonoBehaviour
 {
     private AudioSource _sound;
@@ -54,3 +118,5 @@ public class SecurityAlarm : MonoBehaviour
         _volumeChangeRoutine = StartCoroutine(VolumeChange(speed));
     }
 }
+
+*/
